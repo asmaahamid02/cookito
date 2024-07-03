4e4dc982a96739f54a37fb22acde106535eac321
Asmaa Hamid <asmaahamid002@gmail.com>
2024-07-03 16:47:08 +0300

    added card components

---
+++ resources/views/components/category-card.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ route('categories', '.$category->id.')}}" class="group relative w-full text-gray-800 dark:text-gray-200 hover:text-amber-600 hover:dark:text-amber-400 text-lg font-semibold uppercase text-center bg-gray-200/60 dark:bg-gray-800/60 rounded-xl shadow-lg overflow-hidden">
+    <div class="w-full h-60 sm:h-56 md:h-48 relative overflow-hidden">
+        <img src="{{ url('storage/categories/'.$category['image'])}}" alt="{{$category['name']}}" class="object-cover group-hover:scale-125 transition-transform ease-in-out duration-300">
+    </div>
+    <p class="text-center py-4">{{$category->name}}</p>
+</a>
---
+++ resources/views/components/recipe-card.blade.php
@@ -0,0 +1,30 @@
+<a href="{{ route('recipes', $recipe->id)}}" class="group text-gray-800 dark:text-gray-200 text-lg font-semibold capitalize">
+    <div class="relative overflow-hidden rounded-xl shadow-lg">
+        <div class="w-full h-60 sm:h-56 md:h-48 relative overflow-hidden">
+            <img src="{{ $recipe->image ? url('storage/recipes/' . $recipe->image) : asset('assets/images/placeholders/placeholder.png')}}" alt="{{$recipe->title}}" class="object-cover object-center hover:scale-125 transition-transform ease-in-out duration-300">
+        </div>
+        <div class="p-4 bg-white dark:bg-gray-900 bg-opacity-90">
+            <span class="text-gray-600 dark:text-gray-400 flex">
+                @foreach(range(1,$recipe->average_rating) as $rating)
+                <x-heroicon-s-star class="w-4 h-4 text-amber-600 dark:text-amber-400" />
+                @endforeach
+                @foreach(range(1, 5 - $recipe->average_rating) as $rating)
+                <x-heroicon-o-star class="w-4 h-4" />
+                @endforeach
+            </span>
+            <h4 class="mt-1 text-2xl truncate group-hover:text-amber-600 dark:group-hover:text-amber-400">{{$recipe->title}}</h4>
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400 truncate">By <span class="text-gray-700 dark:text-gray-500">{{$recipe->user->name}}</span></p>
+            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400 line-clamp-2">{{$recipe->description}}</p>
+            <div class="mt-4 flex justify-between items-center text-gray-600 dark:text-gray-400 group-hover:text-amber-600 dark:group-hover:text-amber-400">
+                <p class="flex items-center gap-1">
+                    <x-heroicon-o-clock />
+                    <span class=" text-sm">{{$recipe->cook_time}}</span>
+                </p>
+                <p class="flex items-center gap-1">
+                    <x-heroicon-o-users />
+                    <span class="text-sm">{{$recipe->servings}}</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</a>
