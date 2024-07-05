9bd95b3209740a36e9796a0b1713f50062361c1f
Asmaa Hamid <asmaahamid002@gmail.com>
2024-07-05 06:47:37 +0300

    refactor + theme icon + search form

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,9 +18,6 @@
                     <x-nav-link :href="route('recipes')" :active="request()->routeIs('recipes')">
                         {{ __('Recipes') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
-                        {{ __('About') }}
-                    </x-nav-link>
                     @auth
                     <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
                         <x-heroicon-o-user class="mb-1" />
@@ -39,21 +36,30 @@
             <!-- Submit recipe / Search recipe -->
             <div class="flex items-center ms-6">
                 <!-- Search input -->
-                <div class="relative hidden lg:block">
-                    <x-text-input type="text" name="search" id="search" class="block w-full pl-10 pr-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md leading-5 bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-200 placeholder-gray-500 dark:placeholder-gray-400 sm:text-sm" placeholder="Search recipes..." />
-                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <x-heroicon-o-magnifying-glass class="text-gray-400 dark:text-gray-500" />
-                    </div>
+                <div class="hidden lg:block">
+                    <x-search-form />
                 </div>
 
                 <!-- Search icon for small screens -->
-                <x-icon-button class="inline-flex lg:hidden">
+                <x-icon-button class="inline-flex lg:hidden" id="search-toggle">
                     <x-heroicon-o-magnifying-glass class="text-gray-500 dark:text-gray-400 font-bold w-6 h-6" />
+                    <span class="sr-only">Search recipes</span>
                 </x-icon-button>
 
                 <x-primary-button class="ms-3 hidden sm:inline-flex">
                     {{ __('Submit recipe') }}
                 </x-primary-button>
+
+                <!-- Toggle theme -->
+                <x-icon-button id="theme-toggle" type="button" class="ms-3">
+                    <svg id="toggle-dark-theme-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                    </svg>
+                    <svg id="toggle-light-theme-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="sr-only">Toggle theme</span>
+                </x-icon-button>
 
                 <!-- Hamburger -->
                 <x-icon-button @click="open = ! open" class="md:hidden ms-1 -me-2 inline-flex">
@@ -61,6 +67,8 @@
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
+
+                    <span class="sr-only">Open main menu</span>
                 </x-icon-button>
             </div>
 
@@ -102,11 +110,24 @@
                 <x-responsive-nav-link :href="route('recipes')" :active="request()->routeIs('recipes')">
                     {{ __('Recipes') }}
                 </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
-                    {{ __('About') }}
-                </x-responsive-nav-link>
             </div>
         </div>
     </div>
+
 </nav>
+<div class="h-2 w-full bg-white dark:bg-gray-800 block lg:hidden"></div>
+<!-- Search input for small screens -->
+<div class="hidden lg:hidden bg-white dark:bg-gray-800" id="small-screen-search-form">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <x-search-form />
+    </div>
+</div>
+
+<script>
+    const search_toggle = document.querySelector('#search-toggle');
+    const small_screen_search_form = document.querySelector('#small-screen-search-form');
+
+    search_toggle.addEventListener('click', () => {
+        small_screen_search_form.classList.toggle('hidden');
+    });
+</script>
