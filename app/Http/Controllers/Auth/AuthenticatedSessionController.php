92b3bd418362c70cd803d1504d28a2f9ffbd64e4
Asmaa Hamid <asmaahamid002@gmail.com>
2024-07-18 06:17:27 +0300

    added apiLogin function

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -44,4 +44,19 @@ class AuthenticatedSessionController extends Controller
 
         return redirect('/');
     }
+
+    /**
+     * Handle api authentication request.
+     */
+    public function apiLogin(LoginRequest $request): RedirectResponse | \Illuminate\Http\JsonResponse
+    {
+        $request->validated();
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $access_token = Auth::user()->createToken(Auth::user()->email)->plainTextToken;
+            return response()->json(['message' => 'Authenticated', 'access_token' => $access_token], 200);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 403);
+        }
+    }
 }
