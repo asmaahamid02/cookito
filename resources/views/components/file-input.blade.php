043dc3a33520f1805e7010af428b38a01af7e211
Asmaa Hamid <asmaahamid002@gmail.com>
2024-07-18 06:19:15 +0300

    file input component

---
+++ resources/views/components/file-input.blade.php
@@ -0,0 +1,7 @@
+@props(['disabled' => false, 'rules' => null])
+<input type="file" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+'class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-amber-400 dark:focus:border-amber-500 focus:ring-amber-400 dark:focus:ring-amber-500 rounded-md shadow-sm',
+]) !!} />
+@if ($rules)
+<p class="text-sm text-gray-500 dark:text-gray-400 mt-2">{{ $rules }}</p>
+@endif
