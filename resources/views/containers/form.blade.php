1ae2bfe61e6225be20d10c8142332653ef7224f9
Asmaa Hamid <asmaahamid002@gmail.com>
2024-07-18 06:09:30 +0300

    added form container component

---
+++ resources/views/containers/form.blade.php
@@ -0,0 +1,5 @@
+<section class="py-12 bg-gray-100 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800">
+    <div class="w-full sm:max-w-xl sm:mx-auto mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</section>
